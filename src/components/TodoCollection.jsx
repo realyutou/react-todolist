094db77a48886e80aaabda50a41f0db84f8a3165
realyutou <ssps9430@gmail.com>
2024-04-12 14:43:41 +0800

    feat: add props for TodoCollection, Header, TodoInput

--- src/components/TodoCollection.jsx
+++ src/components/TodoCollection.jsx
@@ -1,14 +1,9 @@
 import TodoItem from './TodoItem';
 
-const TodoCollection = () => {
+const TodoCollection = ({ todos, onToggleDone, onSave, onDelete, onChangeMode }) => {
   return (
     <div>
-      TodoCollection
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
+      {todos.map(todo => <TodoItem key={todo.id} todo={todo} />)}
     </div>
   );
 };
